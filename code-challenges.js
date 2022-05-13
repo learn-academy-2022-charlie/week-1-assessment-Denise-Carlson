f49fd90dc886184168c048a9af8df5435d8029d2
Denise-Carlson <carlsonden@gmail.com>
2022-05-13 15:38:24 -0700

    made branch and started on code challenge questions 1

--- code-challenges.js
+++ code-challenges.js
@@ -16,6 +16,14 @@ const fruit4 = "kiwi"
 
 
 // --------------------1) Create a function that takes in any given number and determines if the number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below. Expected output: "42 is below boiling point", "350 is above boiling point", "212 is at boiling point"
+ //make a function that inputs a number and decides if that number is below boiling point (first condition), at boiling point (2nd condition), above boiling point ( third condition) boiling point is equal to 212 degrees Farenheit. The expected output will be "42 is below boiling point" for temp1 variable , "350 is above boiling point" for tem2 variable , "212 is at boiling point" for temp3 variable. The function needs to be declared in order to get the terminal to output the expected string ( correct words) I need to use 3 conditionals in order for the expected output to occur. This needs to work and be tested using all numbers
+
+const boilingPoint =(num1) => {
+if(num1 < 212) {
+    return `${num1} is below boiling point`
+}
+}
+
 
 const temp1 = 42
 const temp2 = 350
